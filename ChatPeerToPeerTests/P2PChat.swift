147ba80f247ce7c9328f3aa3bc6012250af922f8
Cst3ProjectSwift <cst3.project.swift@gmail.com>
2016-12-02 09:00:42 +0100

    Initial Commit

---
+++ ChatPeerToPeerTests/P2PChat.swift
@@ -0,0 +1,37 @@
+//
+//  P2PChatTests.swift
+//  P2PChat
+//
+//  Created by Josua von Reding on 28-11-2016.
+//  Developed by Josua von Reding and Gian Brun
+//  Copyright (c) 2016 Josua von Reding, Gian Brun. All rights reserved
+//
+
+import UIKit
+import XCTest
+
+class P2PChat: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
